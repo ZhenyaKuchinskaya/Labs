9c455de935186002bc29252e539cfae7356a6064
ZhenyaKuchinskaya <32042574+ZhenyaKuchinskaya@users.noreply.github.com>
2018-01-02 23:54:00 +0300

    Add files via upload

---
+++ WD/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Lab_4__UnitTest_
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driver;
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driver = new ChromeDriver(@"C:\Users\Евгения\source\repos\UnitTestProject1");
+            driver.Manage().Window.Maximize();
+        }
+        [TestMethod]
+        public void Test()
+        {
+            driver.Navigate().GoToUrl("http://github.com");
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.Id("login_field")).SendKeys("JEKA_TEST");
+            driver.FindElement(By.Id("password")).SendKeys("JEKA_TEST");
+            driver.FindElement(By.Name("commit")).Click();
+        }
+    }
+}
